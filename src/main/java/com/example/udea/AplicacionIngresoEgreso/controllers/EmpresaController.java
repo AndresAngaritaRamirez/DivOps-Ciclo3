0e4002d1d0a9f2674d0292ecdf37242f60214755
AndresAngaritaRamirez <78805945+AndresAngaritaRamirez@users.noreply.github.com>
2022-09-26 10:19:25 -0500

    Devolví los cambios

--- src/main/java/com/example/udea/AplicacionIngresoEgreso/controllers/EmpresaController.java
+++ src/main/java/com/example/udea/AplicacionIngresoEgreso/controllers/EmpresaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.view.RedirectView;
 import java.util.List;
 
 @RestController
-@RequestMapping("/empresas")
+@RequestMapping("/Empresas")
 public class EmpresaController {
     public EmpresaService empresaService;
 
